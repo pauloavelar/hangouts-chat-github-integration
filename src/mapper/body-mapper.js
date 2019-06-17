1d47c5ede9f08b2051620a5441322dcb59ed4ed0
Paulo Avelar <paulo.avelar@mercadolivre.com>
2019-06-16 23:31:12 -0300

    [pavelar] Body formatter (bold and italic)

--- src/mapper/body-mapper.js
+++ src/mapper/body-mapper.js
@@ -1,7 +1,21 @@
 module.exports = { mapBody };
 
-const MAX_LENGTH = 255;
+const MAX_LENGTH = 500;
+const REGEX_BOLD = /\*\*(.+)\*\*/g;
+const REGEX_ITALIC = /\*(.+)\*/g;
 
 function mapBody(body = '') {
-  return body.length > MAX_LENGTH ? `${body.substr(0, MAX_LENGTH)}...` : body;
+  const formattedBody = adjustFormatting(body);
+
+  if (formattedBody.length > MAX_LENGTH) {
+    return `${formattedBody.substr(0, MAX_LENGTH)}...`;
+  }
+
+  return formattedBody;
+}
+
+function adjustFormatting(body) {
+  return body
+    .replace(REGEX_BOLD, '<b>$1</b>')
+    .replace(REGEX_ITALIC, '<i>$1</i>');
 }
